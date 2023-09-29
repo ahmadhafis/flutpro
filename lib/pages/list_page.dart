a56cb5164a9d8983dcebf786b859484549b7ca52
ahmadhafis <ahmadhafis296@gmail.com>
2023-09-29 08:08:01 +0700

    list_page

--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ListPage extends StatefulWidget {
   const ListPage({super.key});
@@ -8,6 +9,40 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
+  bool isExpense = true;
+
+  void openDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: SingleChildScrollView(
+              child: Center(
+                child: Column(children: [
+                  Text(
+                    isExpense ?
+                    "Input Data" : "Input data",
+                    style: GoogleFonts.montserrat(fontSize: 18, color: (isExpense) ? Colors.red : Colors.green),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Input here..."),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text("Save"))
+                ]),
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -15,21 +50,26 @@ class _ListPageState extends State<ListPage> {
       children: [
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Card(
-            elevation: 12,
-            child: ListTile(
-              title: Text("Rp. 6.000.000"),
-              subtitle: Text("Gaji"),
-              leading: Container(
-                child: Icon(
-                  Icons.download,
-                  color: Colors.green,
-                ),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(4)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Switch(
+                value: isExpense,
+                onChanged: (bool value) {
+                  setState(() {
+                    isExpense = value;
+                  });
+                },
+                inactiveTrackColor: Colors.green[200],
+                inactiveThumbColor: Colors.green,
+                activeColor: Colors.red,
               ),
-            ),
+              IconButton(
+                  onPressed: () {
+                    openDialog();
+                  },
+                  icon: Icon(Icons.add))
+            ],
           ),
         ),
         Padding(
@@ -37,20 +77,26 @@ class _ListPageState extends State<ListPage> {
           child: Card(
             elevation: 12,
             child: ListTile(
-              title: Text("Rp. 40.000"),
-              subtitle: Text("Makan Siang"),
-              leading: Container(
-                child: Icon(
-                  Icons.upload,
-                  color: Colors.red,
-                ),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(4)),
+              leading: (isExpense)
+                  ? Icon(
+                      Icons.upload,
+                      color: Colors.red,
+                    )
+                  : Icon(Icons.download, color: Colors.green),
+              title: Text("Pengeluaran"),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+                ],
               ),
             ),
           ),
-        ),
+        )
       ],
     ));
   }
